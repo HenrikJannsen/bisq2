a90474b95264417c17101805e3899a9e97924570
HenrikJannsen <boilingfrog@gmx.com>
2024-10-20 00:27:59 +0700

    Add publishing for network-common and network-identity Tor project is failing with that change for unclear reasons

--- network/network-common/build.gradle.kts
+++ network/network-common/build.gradle.kts
@@ -1,4 +1,21 @@
 plugins {
     id("bisq.java-library")
     id("bisq.protobuf")
+    id("maven-publish")
 }
+
+group = "bisq"
+version = "2.1.1"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "network-common"
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
+
--- network/network-identity/build.gradle.kts
+++ network/network-identity/build.gradle.kts
@@ -1,6 +1,22 @@
 plugins {
     id ("bisq.java-library")
     id ("bisq.protobuf")
+    id("maven-publish")
+}
+
+group = "bisq"
+version = "2.1.1"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            artifactId = "network-identity"
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
 }
 
 dependencies {
