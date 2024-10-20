e375133f0cede3582cb68f8f14ef83a09f5e9eb9
HenrikJannsen <boilingfrog@gmx.com>
2024-10-20 13:25:34 +0700

    Revert "Add publishing for network-common and network-identity"
    This reverts commit a90474b95264417c17101805e3899a9e97924570.

--- network/network-common/build.gradle.kts
+++ network/network-common/build.gradle.kts
@@ -1,21 +1,4 @@
 plugins {
     id("bisq.java-library")
     id("bisq.protobuf")
-    id("maven-publish")
 }
-
-group = "bisq"
-version = "2.1.1"
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            artifactId = "network-common"
-        }
-    }
-    repositories {
-        mavenLocal()
-    }
-}
-
--- network/network-identity/build.gradle.kts
+++ network/network-identity/build.gradle.kts
@@ -1,22 +1,6 @@
 plugins {
     id ("bisq.java-library")
     id ("bisq.protobuf")
-    id("maven-publish")
-}
-
-group = "bisq"
-version = "2.1.1"
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            artifactId = "network-identity"
-        }
-    }
-    repositories {
-        mavenLocal()
-    }
 }
 
 dependencies {
